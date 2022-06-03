de2ce4d1c1a1a4d5a558965bb2e4bf02505918a4
gzbrdr <gabrielzbardini@gmail.com>
2022-06-03 00:02:34 -0300

    agora com colisão nas paredes

--- main.js
+++ main.js
@@ -4,16 +4,38 @@ const topBar = document.getElementById('topBar')
 const bottomBar = document.getElementById('bottomBar')
 const bars = document.getElementsByClassName('bar')
 
+const gameWidth = gameScreen.clientWidth
+const gameHeight = gameScreen.clientHeight
+
 const player1 = {width: 0, height: 0, positionX: 0, positionY: 0, velocity: 0, points: 0}
 const player2 = {width: 0, height: 0, positionX: 0, positionY: 0, velocity: 0, points: 0}
 const players = [player1, player2]
 
-const gameWidth = gameScreen.clientWidth
+const ballAtributes = {
+    width: gameWidth/100,
+    height:gameWidth/100,
+    positionX: gameWidth/2, 
+    positionY: gameHeight/2,
+    velocityX: 10,
+    velocityY: 10
+}
+
+ball.style.width = (ballAtributes.width) + 'px'
+ball.style.height = (ballAtributes.height) + 'px'
+ball.style.left = (ballAtributes.positionX - ballAtributes.width/2) + 'px'
+ball.style.top = (ballAtributes.positionY-ballAtributes.width/2) + 'px'
+
+function startGame() {
+    setDefaultBars()
+    setBarsStyle()
+    setBallPosition()
+}
+startGame()
 
 function setDefaultBars(){
     for (barNumber = 0; barNumber < 2; barNumber++){
-        players[barNumber].width = gameWidth/10,                                         
-        players[barNumber].height = gameScreen.clientHeight/100,
+        players[barNumber].width = gameWidth/5,                                         
+        players[barNumber].height = gameHeight/100,
         players[barNumber].positionX = gameWidth/2,
         players[barNumber].velocity = gameWidth/100
     }
@@ -30,6 +52,11 @@ function setBarsStyle() {
         bars[barNumber].style.top = `${players[barNumber].positionY}px`
     }
 }
+ 
+function setBallPosition() {
+    ball.style.left = `${ballAtributes.positionX-ballAtributes.width/2}px`
+    ball.style.top = `${ballAtributes.positionY-ballAtributes.height/2}px`
+}
 
 function movePlayer(player, direction) {
     const playerNextLocation = player.positionX*direction + player.velocity
@@ -42,24 +69,26 @@ function movePlayer(player, direction) {
     else {
         player.positionX = playerNextLocation*direction
     }
-}
-//OBRIGADO SOR ROGER
+}//OBRIGADO SOR ROGER
 
 const keyActions = {
     ArrowLeft(){
-        movePlayer(player1, direction = -1)
+        let left = -1
+        movePlayer(player1, left)
     },
     ArrowRight(){
-        movePlayer(player1, direction = 1)
+        let right = 1
+        movePlayer(player1, right)
     },
     KeyA(){
-        movePlayer(player2, direction = -1)
+        let left = -1
+        movePlayer(player2, left)
     },
     KeyD(){
-        movePlayer(player2, direction = 1)
+        let right = 1
+        movePlayer(player2, right)
     }
-}
-//OBRIGADO FILIPE DESCHAMPS
+}//OBRIGADO FILIPE DESCHAMPS
 
 const handleKeys = {}
 
@@ -76,11 +105,44 @@ document.addEventListener('keyup', (keyUpEvent) => {
     delete handleKeys[keyPressed]
 })
 
-setInterval(function gameFrames() {   
-    for (const objElements in handleKeys) {
-        const keyHandled = handleKeys[objElements]
+function loadBallPosition() {
+    const ballPosX = ballAtributes.positionX += ballAtributes.velocityX
+    const ballPosY = ballAtributes.positionY += ballAtributes.velocityY
+    ball.style.left = ballPosX + 'px'
+    ball.style.top = ballPosY + 'px'
+}
+function checkBallColisions() {
+    function checkWallColision() {
+        const directionX = Math.sign(ballAtributes.velocityX)
+        const ballHitbox = ballAtributes.positionX*directionX + ballAtributes.width/2 + ballAtributes.width/2*directionX
+        const gameLimit = gameWidth / 2 + (gameWidth / 2 * directionX)
+        const ballHitedWall = (ballHitbox >= gameLimit)
+        console.log();
+        if (ballHitedWall) {
+            ballAtributes.velocityX *= -1
+        }
+    }
+    function checklolColision() {
+        const directionY = Math.sign(ballAtributes.velocityY)
+        const ballHitbox = ballAtributes.positionY*directionY + ballAtributes.height/2 + ballAtributes.height/2*directionY
+        const gameLimit = gameHeight / 2 + (gameHeight / 2 * directionY)
+        const ballHitedWall = (ballHitbox >= gameLimit)
+        if (ballHitedWall) {
+            ballAtributes.velocityY *= -1
+        }
+    }
+    checkWallColision()
+    checklolColision()
+}
+
+function loadFrames(){
+    for (const elements in handleKeys) {
+        const keyHandled = handleKeys[elements]
         keyActions[keyHandled]()
     }
     setBarsStyle()
-},17)
-setDefaultBars()    
+    loadBallPosition()
+    checkBallColisions()
+}
+
+setInterval(loadFrames,0.017*1000)
